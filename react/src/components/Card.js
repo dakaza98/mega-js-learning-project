ee51a09c81dcf148ad9289541d3b69c0b16d432a
Daniel Fehrm <daniel@fehrm.se>
2021-07-23 16:14:56 +0200

    made cards clickable

--- react/src/components/Card.js
+++ react/src/components/Card.js
@@ -1,22 +1,43 @@
+import React from "react";
 import "../styles/card.scss";
 
-function Card({ name, imageUrl, totalTimesSeen }) {
-  return (
-    <div className="card">
-      <img src={imageUrl} alt="{name}" />
-      <div className="card-details">
-        <p className="airplane-name">{name}</p>
-        <div className="total-times-container">
-          <p>Total times seen: </p>
-          <div className="total-times-incrementer">
-            <button>-</button>
-            <p>{totalTimesSeen}</p>
-            <button>+</button>
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = props.name;
+    this.imageUrl = props.imageUrl;
+
+    this.state = {
+      totalTimesSeen: props.totalTimesSeen,
+    };
+  }
+
+  increase = () => {
+    this.setState({ totalTimesSeen: this.state.totalTimesSeen + 1 });
+  };
+
+  decrease = () => {
+    this.setState({ totalTimesSeen: this.state.totalTimesSeen - 1 });
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <img src={this.imageUrl} alt="{this.name}" />
+        <div className="card-details">
+          <p className="airplane-name">{this.name}</p>
+          <div className="total-times-container">
+            <p>Total times seen: </p>
+            <div className="total-times-incrementer">
+              <button onClick={this.decrease}>-</button>
+              <p>{this.state.totalTimesSeen}</p>
+              <button onClick={this.increase}>+</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Card;
