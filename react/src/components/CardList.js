d567c95ff8cd8a5c3dc6f3be56d63a589d18ef26
Daniel Fehrm <daniel@fehrm.se>
2021-07-23 16:26:13 +0200

    added new airplanes

--- react/src/components/CardList.js
+++ react/src/components/CardList.js
@@ -1,6 +1,9 @@
 import Card from "./Card";
 import "../styles/cardList.scss";
 import c172Skyhawk from "../images/cessna-172-skyhawk.jpg";
+import b737 from "../images/boeing-737.jpg";
+import aBeluga from "../images/airbus-beluga.jpg";
+import pa28 from "../images/piper-pa28.jpg";
 
 function CardList() {
   return (
@@ -10,21 +13,9 @@ function CardList() {
         imageUrl={c172Skyhawk}
         totalTimesSeen={20}
       />
-      <Card
-        name="Cessna 172 Skyhawk"
-        imageUrl={c172Skyhawk}
-        totalTimesSeen={20}
-      />
-      <Card
-        name="Cessna 172 Skyhawk"
-        imageUrl={c172Skyhawk}
-        totalTimesSeen={20}
-      />
-      <Card
-        name="Cessna 172 Skyhawk"
-        imageUrl={c172Skyhawk}
-        totalTimesSeen={20}
-      />
+      <Card name="Boeing 737" imageUrl={b737} totalTimesSeen={52} />
+      <Card name="Airbus Beluga XL" imageUrl={aBeluga} totalTimesSeen={30} />
+      <Card name="Piper PA-28" imageUrl={pa28} totalTimesSeen={14} />
     </div>
   );
 }
