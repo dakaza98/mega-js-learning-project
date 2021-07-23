91f04c863c251074bd36908e3423f66315d4aa45
Daniel Fehrm <daniel@fehrm.se>
2021-07-23 15:03:55 +0200

    restructure files

---
+++ react/src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/app.scss";
+import Card from "./Card";
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Airplane Spotting</h1>
+      <Card />
+    </main>
+  );
+}
+
+export default App;
---
+++ react/src/components/Card.js
@@ -0,0 +1,11 @@
+import c172Skyhawk from "../images/cessna-172-skyhawk.jpg";
+
+function Card() {
+  return (
+    <div className="card">
+      <img src={c172Skyhawk} alt="Cessna 172 Skyhawk" />
+    </div>
+  );
+}
+
+export default Card;
